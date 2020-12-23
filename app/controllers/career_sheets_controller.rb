b947333298dbf8748e7b996c691e51398dd233ac
taizo-pro <t2s.single@gmail.com>
2020-12-23 17:00:16 +0900

    一覧画面でデータを表示させる

--- app/controllers/career_sheets_controller.rb
+++ app/controllers/career_sheets_controller.rb
@@ -1,5 +1,7 @@
 class CareerSheetsController < ApplicationController
   def index
+    @career_sheets = CareerSheet.all
+    # @career_sheets = CareerSheet.all
   end
 
   def show
@@ -13,8 +15,18 @@ class CareerSheetsController < ApplicationController
   end
 
   def create
+    @career_sheet = CareerSheet.new(career_sheet_params)
+    @career_sheet.save
+    flash[:success] = "登録完了しました"
+    redirect_to root_path
+
   end
 
   def delete
   end
+
+  private
+    def career_sheet_params
+      params.require(:career_sheet).permit!
+    end
 end
